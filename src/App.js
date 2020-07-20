d8b7a3e9b86cc54501174c3e6c15913837b548fb
재준 전 <wjswowns1234@outlook.com>
2020-07-20 23:14:05 +0900

    move_app_start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoding: true,
+    movie: [],
+  };
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ isLoding: false });
+    }, 3000);
+  }
+
+  render() {
+    const { isLoding } = this.state;
+    return <div>{isLoding ? "Loding..." : "We are Ready"}</div>;
+  }
 }
 
 export default App;
