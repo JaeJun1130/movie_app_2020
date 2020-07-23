e63bbb1bb4dfc8bfaa79c3655f490adcad2e5cd0
재준 전 <wjswowns1234@outlook.com>
2020-07-24 01:59:50 +0900

    Navigation

--- src/App.js
+++ src/App.js
@@ -1,59 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
-import Movie from "./Movie";
-import axios from "axios";
+import { HashRouter, Route } from "react-router-dom";
 
-import "./Movie.css";
+import "./App.css";
 
-class App extends React.Component {
-  state = {
-    isLoding: true,
-    movies: [],
-  };
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
-  getmovies = async () => {
-    //api axios를 통해 api 를 가져오는 함수 (async await)
-    const {
-      data: {
-        data: { movies },
-      },
-    } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
-    );
-    this.setState({ movies, isLoding: false });
-  };
-
-  componentDidMount() {
-    //render 끝나고 실행
-    this.getmovies();
-  }
-
-  render() {
-    const { isLoding, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoding ? (
-          <div className="loader">
-            <span className="loader__text">Loding...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/about" exact={true} component={About}></Route>
+    </HashRouter>
+  );
 }
 
 export default App;
---
+++ src/routes/Home.js
@@ -0,0 +1,58 @@
+import React from "react";
+import axios from "axios";
+
+import "./Home.css";
+import Movie from "../components/Movie";
+
+class Home extends React.Component {
+  state = {
+    isLoding: true,
+    movies: [],
+  };
+
+  getmovies = async () => {
+    //api axios를 통해 api 를 가져오는 함수 (async await)
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoding: false });
+  };
+
+  componentDidMount() {
+    //render 끝나고 실행
+    this.getmovies();
+  }
+
+  render() {
+    const { isLoding, movies } = this.state;
+    return (
+      <section className="container">
+        {isLoding ? (
+          <div className="loader">
+            <span className="loader__text">Loding...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
+    );
+  }
+}
+
+export default Home;
