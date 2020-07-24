807ea0256c2f4ffb174e8ca41502d382d851a23e
재준 전 <wjswowns1234@outlook.com>
2020-07-24 17:00:32 +0900

    완성

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import Home from "./routes/Home";
 import About from "./routes/About";
 import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/about" exact={true} component={About}></Route>
+      <Route path="/movie/:id" exact={true} component={Detail}></Route>
     </HashRouter>
   );
 }
